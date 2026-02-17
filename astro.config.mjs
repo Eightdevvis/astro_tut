36f49d6bbf8860e1bf9e81f610d4cde2f32054c3
Sasha <sevendevs@outlook.com>
2026-02-17 15:41:14 +0100

    finish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astrotutut.netlify.app"
+  site: "https://astrotutut.netlify.app",
+  integrations: [preact()]
 });
