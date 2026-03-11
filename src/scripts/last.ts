e59dc88fecf6cc60af8e329b96f6c6cfbd148330
Eightdevvis <sevendevs@outlook.com>
2026-03-11 12:07:39 +0100

    jojojojo

---
+++ src/scripts/last.ts
@@ -0,0 +1,14 @@
+
+
+const allPosts = Object.values(import.meta.glob('../pages/posts/*.md', { eager: true }));
+
+
+const lastPost = allPosts[allPosts.length -1];
+
+function writePostOrder(postArr: Array<any>): void{
+    console.log(postArr);
+}
+
+writePostOrder(allPosts);
+
+export default lastPost;
